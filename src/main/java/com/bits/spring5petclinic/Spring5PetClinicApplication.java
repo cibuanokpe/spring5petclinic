b26f1a8191db5222e7037cc57e07fc6165d5aea5
cibuanokpe <clement_ibuanokpe@hotmail.com>
2020-08-19 23:14:07 +0100

    Renamed main and main test classes

--- src/main/java/com/bits/spring5petclinic/Spring5PetClinicApplication.java
+++ src/main/java/com/bits/spring5petclinic/Spring5PetClinicApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring5petclinicApplication {
+public class Spring5PetClinicApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring5petclinicApplication.class, args);
+        SpringApplication.run(Spring5PetClinicApplication.class, args);
     }
 
 }
--- src/test/java/com/bits/spring5petclinic/Spring5PetClinicApplicationTests.java
+++ src/test/java/com/bits/spring5petclinic/Spring5PetClinicApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Spring5petclinicApplicationTests {
+class Spring5PetClinicApplicationTests {
 
     @Test
     void contextLoads() {

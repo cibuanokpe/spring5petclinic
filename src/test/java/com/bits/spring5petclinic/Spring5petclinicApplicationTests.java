7e7d1d0fc7196e12be2fe08e7c0c81c73cb2d3ff
cibuanokpe <clement_ibuanokpe@hotmail.com>
2020-08-19 23:10:00 +0100

    Initial version add again

---
+++ src/test/java/com/bits/spring5petclinic/Spring5petclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bits.spring5petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5petclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
